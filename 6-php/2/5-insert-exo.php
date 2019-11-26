5cfbebc00b3763ac43048d3953a5cb10f63569e7
toto <toto@mail.fr>
2019-11-26 15:03:06 +0100

    bdd update

--- 6-php/2/5-insert-exo.php
+++ 6-php/2/5-insert-exo.php
@@ -1,5 +1,40 @@
 <?php
+    if (isset($_POST['btn_register'])) {
+        // récupérer les valeurs
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
 
+        // vérifier si les valeurs sont ok
+        $errors = [];
+        if ($name == null || $name == "") {
+            $errors[] = "Veuillez saisir un nom";
+        }
+
+        if ($email == null || ($email != "" && !filter_var($email, FILTER_VALIDATE_EMAIL))) {
+            $errors[] = "Veuillez saisir une adresse correcte";
+        }
+
+        // enregistrer en bdd le nouvel utilisateur
+        if (count($errors) == 0) {
+            // 1- connexion serveur bdd et choix de la base
+            $pdo = new PDO("mysql:host=localhost;dbname=formation_m2i;charset=utf8", "root", "");
+            $sql = "INSERT INTO utilisateur(name, email, is_enabled)
+                    VALUES (:name, :mail, :isEnabled)";
+            $statement = $pdo->prepare($sql);
+            $result = $statement->execute([
+                ':name' => $name,
+                ':mail' => $email,
+                ':isEnabled' => 1
+            ]);
+
+            if ($result) {
+                $success = true;
+            }
+        }
+        else {
+
+        }
+    }
 ?>
 
 <!doctype html>
@@ -22,6 +57,7 @@
 <body>
     <h2>Créer un nouveau utilisateur</h2>
 
+    <p>
     Créer le formulaire html qui permet d'enregister un utilisateur en lui demandant son nom, son email.
 
     Pour le nom : minimum 3 caractères
@@ -30,7 +66,27 @@
 
     En PHP, récup les données, les vérifier, les enregister
     Laisser mysql mettre sa date de création par défaut
-    Activer le compte utilisateur par défaut : donc is_enabled à 1
+    Activer le compte utilisateur par défaut : donc is_enabled à 1</p>
+
+    <?php
+        if (isset($errors)) {
+            foreach ($errors as $error) {
+                echo $error."<br>";
+            }
+        }
+
+        if (isset($success)) {
+            echo "Merci pour votre inscription";
+        }
+    ?>
+
+
+    <form method="post">
+        <input type="text" name="name" placeholder="Votre nom" required/>
+        <input type="email" name="email" placeholder="Votre email" />
+
+        <input type="submit" name="btn_register" />
+    </form>
 </body>
 </html>
 
---
+++ 6-php/2/6-update.php
@@ -0,0 +1,70 @@
+<?php
+    $id = filter_input(INPUT_GET, 'id');
+
+    // si l'id n'a pas été passé en paramètre, on va considérer que cette page retourne une page d'erreur 404
+    if ($id == null || $id == "") {
+        header('HTTP/1.0 404 Not Found');
+        exit;
+    }
+
+    // 1 - récupérateur le player concerné
+    $pdo = new PDO("mysql:host=localhost;dbname=formation_m2i;charset=utf8", "root", "");
+    $sql = "SELECT * FROM player WHERE id = :id";
+    $statement = $pdo->prepare($sql);
+    $result = $statement->execute([':id' => $id]);
+    // on peut récupérer uniquement le premier enregistrement grâce à fetch à la place de fetchAll
+    $player = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($player == false) {
+        $unknownPlayer = true;
+    }
+
+    // 2 - modifier le player
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        table td {
+            border:1px solid pink;
+        }
+    </style>
+</head>
+<body>
+    <h2>Modifier un player</h2>
+
+    <?php
+        if (isset($unknownPlayer)) {
+            echo "Désolé, cet utilisateur n'existe plus ou pas";
+        }
+    ?>
+    <form method="post">
+        <input type="text" name="name" placeholder="Votre nom" required value="<?php echo $player['name']; ?>"/>
+
+        <select name="team" required>
+            <option></option>
+            <option value="1" <?php if ($player['team'] == 1) { echo "selected"; } ?>>Team rouge</option>
+            <option value="2" <?php if ($player['team'] == 2) { echo "selected"; } ?>>Team bleue</option>
+            <option value="3" <?php if ($player['team'] == 3) { echo "selected"; } ?>>Team jaune</option>
+            <option value="4" <?php if ($player['team'] == 4) { echo "selected"; } ?>>Team orange</option>
+        </select>
+
+        <input type="email" name="email" placeholder="Votre email" value="<?php echo $player['mail']; ?>"/>
+
+        <input type="text" name="zipcode" placeholder="Votre code postal" required pattern="\d{5}" value="<?php echo $player['zipcode']; ?>"/>
+
+        <input type="submit" name="btn_register" />
+    </form>
+</body>
+</html>
+
+
